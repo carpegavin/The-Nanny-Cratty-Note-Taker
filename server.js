575bed97dc6fab64f74f7e27cc3382df56848e24
Gavin Phill <carpe.gavin@gmail.com>
2021-01-04 15:32:05 -0600

    added: app.use express to allow it to find the assets; app.post using math floor to assign a unique, random ID to each note

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.get('/notes', function(req, res) {
   res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
-
-
 //----GET from db.json------------------------------
 app.get('/api/notes', function(req, res) {
   res.json(dbJSON);
@@ -72,25 +70,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
-  // Copy request body and generate ID
-  // const note = {...req.body, id: uuidv4()}
-
-  // Push note to dbJSON array - saves data in memory
-  // dbJSON.push(note);
-
-  // Saves data to file by persisting in memory variable dbJSON to db.json file.
-  // This is needed because when we turn off server we loose all memory data like pbJSON variable.
-  // Saving to file allows us to read previous notes (before server was shutdown) from file.
-  // fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(dbJSON), (err) => {
-  //   if (err) {
-  //     return res.json({error: "Error writing to file"});
-  //   }
-
-  //   return res.json(note);
-  // });
-
-
-
+  
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
